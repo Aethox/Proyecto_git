0aac8e096912ef9f5768c077a61a92de412ee482
Steven Buritica <stevenburitica@usantotomas.edu.co>
2021-10-13 09:20:11 -0500

    Tercera Version

---
+++ src/OtraClase.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AMD RYZEN
+ */
+public class OtraClase {
+    private int edad;
+    public OtraClase (int edad){
+        this.edad = edad;
+    }
+}
